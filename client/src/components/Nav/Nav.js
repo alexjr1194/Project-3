f7aedf2da35c72ee78e5388ad1dbe059b92c4365
alexjr1194 <alexjr1194@me.com>
2018-01-06 11:20:00 -0800

    adding test auth

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,8 +5,8 @@ class Nav extends Component {
     return (
       <header className='jumbotron'>
         <h1 className='display-3'>Project-3</h1>
-        <div className="collapse navbar-collapse navbar-right" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
+        <div className=" navbar navbar-right" id="navbarNavAltMarkup">
+          <div className="navbar-tab">
             <a className="nav-item nav-link active" href="/">Home <span className="sr-only">(current)</span></a>
             <a className="nav-item nav-link" href="/donate">Donate</a>
             <a className="nav-item nav-link" href="/view">Donatations</a>
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,97 @@
+const User = require('../models/donor.js');
 const router = require("express").Router();
 const charityController = require("../controllers/charityController.js");
 const donorController = require("../controllers/donorController.js");
 const donationController = require("../controllers/donationController.js");
 
-router.route("/donor/:donor")
-  .get(donorController.findDonor)
-  .post(donorController.createDonor)
+module.exports = function(app, passport){
+	app.get('/', function(req, res){
+		res.render('../client/public/index.html');
+	});
 
-router.route("/donor/populate/:donor")
-  .get(donorController.populatedDonor)
+	app.get('/login', function(req, res){
+		res.render('../client/public/index.html', { message: req.flash('loginMessage') });
+	});
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect: '/profile',
+		failureRedirect: '/login',
+		failureFlash: true
+	}));
 
-router.route("/charity/:id")
-  .get(charityController.findCharity)
-  .post(charityController.createCharity)
+	app.get('/signup', function(req, res){
+		res.render('../client/public/index.html', { message: req.flash('signupMessage') });
+	});
 
-router.route("/donation")
-  .post(donationController.createDonation)
 
-router.route("/donation/:donor")
-  .get(donationController.findDonations)
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect: '/',
+		failureRedirect: '/signup',
+		failureFlash: true
+	}));
 
-router.route("/donation/:charity/available")
-  .get(donationController.findActiveDonations)
+	app.get('/profile', isLoggedIn, function(req, res){
+		res.render('../client/public/index.html', { user: req.user });
+	});
 
-router.route("/donation/activedonation/:id")
-  .get(donationController.findOneDonation)
-  .put(donationController.findOneAndUpdate)
 
-module.exports = router;
+
+	app.get('/:username/:password', function(req, res){
+		var newUser = new User();
+		newUser.local.username = req.params.username;
+		newUser.local.password = req.params.password;
+		console.log(newUser.local.username + " " + newUser.local.password);
+		newUser.save(function(err){
+			if(err)
+				throw err;
+		});
+		res.send("Success!");
+	});
+
+	app.get('/logout', function(req, res){
+		req.logout();
+		res.redirect('/');
+	})
+};
+
+function isLoggedIn(req, res, next) {
+	if(req.isAuthenticated()){
+		return next();
+	}
+
+	res.redirect('/login');
+}
+
+
+
+
+
+
+
+
+
+//
+// router.route("/donor/:donor")
+//   .get(donorController.findDonor)
+//   .post(donorController.createDonor)
+//
+// router.route("/donor/populate/:donor")
+//   .get(donorController.populatedDonor)
+//
+// router.route("/charity/:id")
+//   .get(charityController.findCharity)
+//   .post(charityController.createCharity)
+//
+// router.route("/donation")
+//   .post(donationController.createDonation)
+//
+// router.route("/donation/:donor")
+//   .get(donationController.findDonations)
+//
+// router.route("/donation/:charity/available")
+//   .get(donationController.findActiveDonations)
+//
+// router.route("/donation/activedonation/:id")
+//   .get(donationController.findOneDonation)
+//   .put(donationController.findOneAndUpdate)
+//
+// module.exports = router;
